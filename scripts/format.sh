ea066032877f13640fec3b059531dc0bf78f97ad
John Berlin <n0tan3rd@gmail.com>
2019-03-22 20:10:48 -0400

    poetry now doesnt complain

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-black -v src/crawlmanager
+black -v crawlmanager
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,15 +2,15 @@
 
 case "$1" in
     "types"*)
-     mypy --config-file mypy.ini src/crawlmanager/
+     mypy --config-file mypy.ini crawlmanager/
     ;;
 
     "lint"*)
-     flake8 src/crawlmanager/
+     flake8 crawlmanager/
     ;;
 
     *)
      printf "Checking Typing And Linting\n"
-     flake8 -v --mypy-config mypy.ini src/crawlmanager/
+     flake8 -v --mypy-config mypy.ini crawlmanager/
     ;;
 esac
