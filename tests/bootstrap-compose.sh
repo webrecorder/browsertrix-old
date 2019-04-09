cdc1fb8cdf701feb26365b6d70cf3bd1d20c51bc
Ilya Kreymer <ikreymer@gmail.com>
2019-04-09 14:50:20 -0700

    use separate test-docker-compose.yml for live integration tests

--- tests/bootstrap-compose.sh
+++ tests/bootstrap-compose.sh
@@ -2,11 +2,9 @@
 
 CURR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-export PYWB_COMMAND_LINE="wayback --live --proxy coll"
-
 mkdir $CURR_DIR/webarchive
 wget https://s3.amazonaws.com/webrecorder-builds/crawlmanager-tests/testcoll.tar.gz
 tar xvfz testcoll.tar.gz --directory $CURR_DIR/webarchive
 
-docker-compose -f $CURR_DIR/../docker-compose.yml up -d
+docker-compose -f $CURR_DIR/test-docker-compose.yml up -d
 
